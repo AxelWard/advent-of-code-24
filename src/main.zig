a7687cd4a4e19fad77396d85d9c2451ec2692991
Axel Ward <axel@doghousecreative.co>
2024-12-05 10:14:51 -0700

    Day 5 part 1 complete

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const day_one = @import("./day_one/day_one.zig");
 const day_two = @import("./day_two/day_two.zig");
 const day_three = @import("./day_three/day_three.zig");
 const day_four = @import("./day_four/day_four.zig");
+const day_five = @import("./day_five/day_five.zig");
 
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
@@ -10,4 +11,5 @@ pub fn main() !void {
     try day_two.run(allocator);
     try day_three.run(allocator);
     try day_four.run(allocator);
+    try day_five.run(allocator);
 }
