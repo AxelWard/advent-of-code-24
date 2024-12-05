0a0de2e9145425c12b447987a2542631267e4e8c
Axel Ward <axel@doghousecreative.co>
2024-12-05 13:17:04 -0700

    Day 5 part 2 done

---
+++ src/day_five/REFLECTION.md
@@ -0,0 +1,14 @@
+Alrighty. Day 5. This was definitely another pretty big step up from the previous days. It took me
+about 2 hours to get through this one, although part of that was due to using the wrong buffer when
+I was trying to read through the input.
+
+My solutions today allowed me to explore memory managment a bit more, as I had to deal with parsing
+in a set of rules and then check those rules to check if the inputs were valid. I also spent some
+more time with iterators and arrays, which feels like great practice for future days and projects.
+
+Overall I think I'm starting to settle into using Zig and getting more comfortable with low level
+functionality.
+
++2 stars, emphatically.
+
+See you tomorrow gamers.
--- src/day_five/day_five.zig
+++ src/day_five/day_five.zig
@@ -14,68 +14,67 @@ pub fn run(allocator: std.mem.Allocator) !void {
     var lines = std.mem.splitSequence(u8, buffer[0..input_length], "\n");
 
     var valid_total: usize = 0;
-    var rules = std.StringHashMap([][]const u8).init(allocator);
+    var reordered_total: usize = 0;
 
-    var current_rule: ?[]const u8 = null;
-    var current_rules = std.ArrayList([]const u8).init(allocator);
+    var rules = std.StringHashMap([][]const u8).init(allocator);
 
     while (lines.next()) |line| {
         if (line.len != 0) {
             var line_chunks = std.mem.splitSequence(u8, line, "|");
-            const before = line_chunks.first();
+            const first_chunk = line_chunks.first();
             const second_chunk = line_chunks.next();
 
-            if (second_chunk) |after| {
-                if (current_rule) |current| {
-                    if (!std.mem.eql(u8, current, after)) {
-                        if (rules.fetchRemove(current)) |old_rules| {
-                            try current_rules.appendSlice(old_rules.value);
-                        }
-
-                        if (current_rules.items.len != 0) {
-                            try rules.put(current, try current_rules.toOwnedSlice());
-                        }
-
-                        current_rule = after;
-                    }
-                } else {
-                    current_rule = after;
-                }
-
-                try current_rules.append(before);
+            if (second_chunk) |second_page| {
+                try add_rule(first_chunk, second_page, &rules, allocator);
             } else {
-                if (current_rule) |current| {
-                    if (current_rules.items.len != 0) {
-                        try rules.put(current, try current_rules.toOwnedSlice());
-                        current_rule = null;
-                    }
+                if (try check_print_valid(first_chunk, &rules, allocator)) {
+                    valid_total += try get_middle_page(first_chunk, allocator);
+                } else {
+                    const new_pages = try get_reordered_pages(first_chunk, &rules, allocator);
+                    defer allocator.free(new_pages);
+                    reordered_total += try get_middle_page(new_pages, allocator);
                 }
-
-                valid_total += try check_print_valid(before, &rules, allocator);
             }
         }
     }
 
     std.debug.print("Total of valid middle pages (part 1): {}\n", .{valid_total});
+    std.debug.print("Total of reordered middle pages (part 2): {}\n", .{reordered_total});
 }
 
-fn check_print_valid(print: []const u8, rules: *std.StringHashMap([][]const u8), allocator: std.mem.Allocator) !usize {
-    if (print.len == 0 or rules.count() == 0) return 0;
+fn add_rule(
+    first_page: []const u8,
+    second_page: []const u8,
+    rules: *std.StringHashMap([][]const u8),
+    allocator: std.mem.Allocator,
+) !void {
+    var new_rules = std.ArrayList([]const u8).init(allocator);
+
+    if (rules.fetchRemove(second_page)) |old_rules| {
+        try new_rules.appendSlice(old_rules.value);
+    }
+
+    try new_rules.append(first_page);
+
+    try rules.put(second_page, try new_rules.toOwnedSlice());
+}
+
+fn check_print_valid(
+    print: []const u8,
+    rules: *std.StringHashMap([][]const u8),
+    allocator: std.mem.Allocator,
+) !bool {
+    if (print.len == 0 or rules.count() == 0) return false;
 
     var pages = std.mem.splitSequence(u8, print, ",");
 
     var pages_not_allowed = std.ArrayList([]const u8).init(allocator);
     defer pages_not_allowed.deinit();
 
-    var page_list = std.ArrayList([]const u8).init(allocator);
-    defer page_list.deinit();
-
     while (pages.next()) |page| {
         if (array_list_contains_value(&pages_not_allowed, page)) {
-            return 0;
+            return false;
         }
-
-        try page_list.append(page);
 
         if (rules.get(page)) |page_rules| {
             for (page_rules) |rule| {
@@ -86,12 +85,84 @@ fn check_print_valid(print: []const u8, rules: *std.StringHashMap([][]const u8),
         }
     }
 
+    return true;
+}
+
+fn get_reordered_pages(print: []const u8, rules: *std.StringHashMap([][]const u8), allocator: std.mem.Allocator) ![]const u8 {
+    var pages = std.mem.splitSequence(u8, print, ",");
+    var page_list = std.ArrayList([]const u8).init(allocator);
+
+    var relevant_rules = std.StringHashMap([][]const u8).init(allocator);
+
+    while (pages.next()) |page| {
+        var rules_iterator = relevant_rules.iterator();
+        var earlier_pages = std.ArrayList([]const u8).init(allocator);
+
+        while (rules_iterator.next()) |rules_to_check| {
+            if (array_contains_value(rules_to_check.value_ptr, page)) {
+                try earlier_pages.append(rules_to_check.key_ptr.*);
+            }
+        }
+
+        var insert_index = page_list.items.len;
+        for (page_list.items, 0..) |item, index| {
+            if (array_list_contains_value(&earlier_pages, item)) {
+                insert_index = index;
+                break;
+            }
+        }
+
+        if (insert_index == page_list.items.len) {
+            try page_list.append(page);
+        } else {
+            const old_pages = try page_list.toOwnedSlice();
+            try page_list.appendSlice(old_pages[0..insert_index]);
+            try page_list.append(page);
+            try page_list.appendSlice(old_pages[insert_index..]);
+            allocator.free(old_pages);
+        }
+
+        if (rules.get(page)) |page_rules| {
+            try relevant_rules.put(page, page_rules);
+        }
+    }
+
+    const final_pages = try page_list.toOwnedSlice();
+    defer allocator.free(final_pages);
+
+    var page_string = std.ArrayList(u8).init(allocator);
+    for (final_pages, 1..) |page_value, index| {
+        try page_string.appendSlice(page_value);
+        if (index != final_pages.len) try page_string.append(',');
+    }
+
+    const final_str = try page_string.toOwnedSlice();
+
+    return final_str;
+}
+
+fn get_middle_page(print: []const u8, allocator: std.mem.Allocator) !usize {
+    var pages = std.mem.splitSequence(u8, print, ",");
+
+    var page_list = std.ArrayList([]const u8).init(allocator);
+    defer page_list.deinit();
+
+    while (pages.next()) |page| try page_list.append(page);
+
     if (page_list.items.len != 0) {
         const middle = (page_list.items.len - 1) / 2;
         return try std.fmt.parseUnsigned(usize, page_list.items[middle], 10);
     }
 
     return 0;
+}
+
+fn array_contains_value(array: *[][]const u8, value: []const u8) bool {
+    for (array.*) |item| {
+        if (std.mem.eql(u8, item, value)) return true;
+    }
+
+    return false;
 }
 
 fn array_list_contains_value(list: *std.ArrayList([]const u8), value: []const u8) bool {
