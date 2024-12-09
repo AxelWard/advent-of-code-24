a171ac0ab3f997fb9b1bee0c714f5ecd40d39fa5
Axel Ward <axel@doghousecreative.co>
2024-12-09 09:57:16 -0700

    Refactor a bit to prep for part 2

--- src/day_nine/day_nine.zig
+++ src/day_nine/day_nine.zig
@@ -1,32 +1,68 @@
 const std = @import("std");
-const file = @import("../file-helpers.zig");
+const readFileToBuffer = @import("../file-helpers.zig").readFileToBuffer;
+
+const File = struct {
+    id: ?usize,
+    length: usize,
+};
 
 pub fn run(allocator: std.mem.Allocator) !void {
     const buffer = try allocator.alloc(u8, 20000);
     defer allocator.free(buffer);
-    const input_length = try file.readFileToBuffer("input/day9.txt", buffer);
+    const input_length = try readFileToBuffer("input/day9.txt", buffer);
+
+    var files = std.ArrayList(File).init(allocator);
+    var current_index: usize = 0;
+    while (current_index * 2 < input_length - 1) : (current_index += 1) {
+        try files.append(File{
+            .id = current_index,
+            .length = try std.fmt.parseInt(
+                usize,
+                buffer[current_index * 2 .. current_index * 2 + 1],
+                10,
+            ),
+        });
+
+        if (current_index * 2 + 1 < input_length - 1) try files.append(File{
+            .id = null,
+            .length = try std.fmt.parseInt(
+                usize,
+                buffer[current_index * 2 + 1 .. current_index * 2 + 2],
+                10,
+            ),
+        });
+    }
+
+    const input_files = try files.toOwnedSlice();
+    defer allocator.free(input_files);
 
     std.debug.print("Total file checksum (part 1): {}\n", .{
-        try calculateChecksumFragmented(buffer[0 .. input_length - 1]),
+        try calculateChecksumFragmented(input_files),
     });
-    
+
     std.debug.print("Total file checksum (part 2): {}\n", .{
         try calculateChecksumContiguous(buffer[0 .. input_length - 1]),
     });
 }
 
-fn calculateChecksumFragmented(input: []const u8) !usize {
+fn calculateChecksumFragmented(input: []const File) !usize {
     var left_index: usize = 0;
-    var left_file_id: usize = 0;
+    while (input[left_index].id == null) {
+        left_index += 1;
+    }
+    var left_file_id: usize = input[left_index].id.?;
+
     var right_index = input.len - 1;
-    var right_file_id = right_index / 2;
-    var number_to_take = try std.fmt.parseInt(usize, input[right_index .. right_index + 1], 10);
+    while (input[right_index].id == null) {
+        right_index -= 1;
+    }
+    var right_file_id = input[right_index].id.?;
+    var number_to_take = input[right_index].length;
 
     var block_index: usize = 0;
     var checksum: usize = 0;
     while (left_file_id < right_file_id) {
-        const block_length = try std.fmt.parseInt(usize, input[left_index .. left_index + 1], 10);
-
+        const block_length = input[left_index].length;
         for (block_index..(block_index + block_length)) |mult_index| {
             checksum += left_file_id * mult_index;
         }
@@ -34,14 +70,14 @@ fn calculateChecksumFragmented(input: []const u8) !usize {
 
         left_index += 1;
 
-        var take = try std.fmt.parseInt(usize, input[left_index .. left_index + 1], 10);
+        var take = input[left_index].length;
         while (take > 0) {
             if (number_to_take == 0) {
-                right_file_id -= 1;
+                right_index -= 2;
+                right_file_id = input[right_index].id.?;
 
                 if (right_file_id != left_file_id) {
-                    right_index -= 2;
-                    number_to_take = try std.fmt.parseInt(usize, input[right_index .. right_index + 1], 10);
+                    number_to_take = input[right_index].length;
                 } else {
                     break;
                 }
@@ -65,6 +101,7 @@ fn calculateChecksumFragmented(input: []const u8) !usize {
 }
 
 fn calculateChecksumContiguous(input: []const u8) !usize {
-    if (input.len == 0) return 0;
+    if (input.len == 0) return 0;    
+
     return 0;
 }
