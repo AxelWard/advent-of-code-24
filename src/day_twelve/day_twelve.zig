be36bd80f77858cf311993b98c40172f12bc4bad
Axel Ward <axel@doghousecreative.co>
2024-12-13 15:36:58 -0700

    Day 12 part 1 majorly simplified

--- src/day_twelve/day_twelve.zig
+++ src/day_twelve/day_twelve.zig
@@ -4,6 +4,22 @@ const readFileToBuffer = @import("../file-helpers.zig").readFileToBuffer;
 const Cell = struct {
     plant: u8,
     plot_index: usize,
+
+    fn plantMatches(self: *Cell, rhs: ?*Cell) bool {
+        if (rhs) |check_cell| {
+            return self.plant == check_cell.plant;
+        }
+
+        return false;
+    }
+
+    fn plotMatches(self: *Cell, rhs: ?*Cell) bool {
+        if (rhs) |check_cell| {
+            return self.plot_index == check_cell.plot_index;
+        }
+
+        return false;
+    }
 };
 
 const Grid = @import("../Grid.zig").Grid(Cell);
@@ -20,12 +36,12 @@ pub fn run(allocator: std.mem.Allocator) !void {
     const sizes = try getGardenPlotSizes(&grid, allocator);
     defer {
         allocator.free(sizes.areas);
-        allocator.free(sizes.perimiters);
+        allocator.free(sizes.perimeters);
     }
 
     var total_plot_price: usize = 0;
     for (sizes.areas, 0..) |area, index| {
-        total_plot_price += area * sizes.perimiters[index];
+        total_plot_price += area * sizes.perimeters[index];
     }
 
     std.debug.print("Total garden plot price (part 1): {}\n", .{total_plot_price});
@@ -60,77 +76,82 @@ fn readGrid(buffer: []const u8, allocator: std.mem.Allocator) !Grid {
     };
 }
 
+const PlotInfos = struct {
+    next_plot_index: usize,
+    area_list: std.ArrayList(usize),
+    perimeter_list: std.ArrayList(usize),
+
+    fn addNewPlot(self: *PlotInfos, init_cell: *Cell) !void {
+        init_cell.plot_index = self.next_plot_index;
+        self.next_plot_index += 1;
+
+        try self.area_list.append(1);
+        try self.perimeter_list.append(4);
+    }
+
+    fn combineGridPlots(self: *PlotInfos, grid: *Grid, destination_plot: usize, remove_plot: usize) void {
+        for (0..grid.cells.len) |index| {
+            if (grid.cells[index].plot_index == remove_plot) {
+                grid.cells[index].plot_index = destination_plot;
+            }
+        }
+
+        self.area_list.items[destination_plot] += self.area_list.items[remove_plot];
+        self.perimeter_list.items[destination_plot] += self.perimeter_list.items[remove_plot];
+
+        self.area_list.items[remove_plot] = 0;
+        self.perimeter_list.items[remove_plot] = 0;
+    }
+};
+
 fn getGardenPlotSizes(grid: *Grid, allocator: std.mem.Allocator) !struct {
     areas: []usize,
-    perimiters: []usize,
+    perimeters: []usize,
 } {
-    var area_list = std.ArrayList(usize).init(allocator);
-    var perimiter_list = std.ArrayList(usize).init(allocator);
+    var infos = PlotInfos{
+        .next_plot_index = 0,
+        .area_list = std.ArrayList(usize).init(allocator),
+        .perimeter_list = std.ArrayList(usize).init(allocator),
+    };
 
-    var next_plot_index: usize = 0;
     for (0..grid.height) |y_index| {
         for (0..grid.width) |x_index| {
-            const current_point = Point{ .x = @as(isize, @intCast(x_index)), .y = @as(isize, @intCast(y_index)) };
+            const current_point = Point{
+                .x = @as(isize, @intCast(x_index)),
+                .y = @as(isize, @intCast(y_index)),
+            };
 
             if (grid.getCell(current_point)) |cell| {
                 const x_check = grid.getCell(current_point.add(Point{ .x = -1, .y = 0 }));
                 const y_check = grid.getCell(current_point.add(Point{ .x = 0, .y = -1 }));
 
-                if (x_check) |x_check_cell| {
-                    if (y_check) |y_check_cell| {
-                        if (cell.plant == x_check_cell.plant and
-                            cell.plant == y_check_cell.plant and
-                            y_check_cell.plot_index != x_check_cell.plot_index)
-                        {
-                            cell.plot_index = x_check_cell.plot_index;
-                            const change_index = y_check_cell.plot_index;
-                            for (0..grid.cells.len) |index| {
-                                if (grid.cells[index].plot_index == change_index) {
-                                    grid.cells[index].plot_index = x_check_cell.plot_index;
-                                }
-                            }
+                if (cell.plantMatches(x_check) and cell.plantMatches(y_check)) {
+                    cell.plot_index = x_check.?.plot_index;
 
-                            area_list.items[x_check_cell.plot_index] += area_list.items[change_index] + 1;
-                            perimiter_list.items[x_check_cell.plot_index] += perimiter_list.items[change_index];
-                            area_list.items[change_index] = 0;
-                            perimiter_list.items[change_index] = 0;
-                            continue;
-                        } else if (cell.plant == x_check_cell.plant and cell.plant == y_check_cell.plant) {
-                            cell.plot_index = x_check_cell.plot_index;
-                            area_list.items[x_check_cell.plot_index] += 1;
-                            continue;
-                        }
+                    if (x_check.?.plot_index != y_check.?.plot_index) {
+                        infos.combineGridPlots(grid, x_check.?.plot_index, y_check.?.plot_index);
                     }
-                }
 
-                if (x_check) |x_check_cell| {
-                    if (cell.plant == x_check_cell.plant) {
-                        cell.plot_index = x_check_cell.plot_index;
-                        area_list.items[x_check_cell.plot_index] += 1;
-                        perimiter_list.items[x_check_cell.plot_index] += 2;
-                        continue;
-                    }
-                }
+                    infos.area_list.items[cell.plot_index] += 1;
+                } else if (cell.plantMatches(x_check)) {
+                    cell.plot_index = x_check.?.plot_index;
 
-                if (y_check) |y_check_cell| {
-                    if (cell.plant == y_check_cell.plant) {
-                        cell.plot_index = y_check_cell.plot_index;
-                        area_list.items[y_check_cell.plot_index] += 1;
-                        perimiter_list.items[y_check_cell.plot_index] += 2;
-                        continue;
-                    }
-                }
+                    infos.area_list.items[cell.plot_index] += 1;
+                    infos.perimeter_list.items[cell.plot_index] += 2;
+                } else if (cell.plantMatches(y_check)) {
+                    cell.plot_index = y_check.?.plot_index;
 
-                cell.plot_index = next_plot_index;
-                next_plot_index += 1;
-                try area_list.append(1);
-                try perimiter_list.append(4);
+                    infos.area_list.items[cell.plot_index] += 1;
+                    infos.perimeter_list.items[cell.plot_index] += 2;
+                } else {
+                    try infos.addNewPlot(cell);
+                }
             }
         }
     }
 
     return .{
-        .areas = try area_list.toOwnedSlice(),
-        .perimiters = try perimiter_list.toOwnedSlice(),
+        .areas = try infos.area_list.toOwnedSlice(),
+        .perimeters = try infos.perimeter_list.toOwnedSlice(),
     };
 }
