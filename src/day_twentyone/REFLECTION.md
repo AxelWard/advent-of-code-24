cd9933c7316b818f087f12804c9f21400f8e3c81
Axel Ward <axel@doghousecreative.co>
2025-01-02 16:18:17 -0700

    Day twenty one complete

---
+++ src/day_twentyone/REFLECTION.md
@@ -0,0 +1,13 @@
+Boy howdy... Day twenty one almost did me in. I first started with a massively overly-complicated
+solution that I show off a bit in my whiteboard photo for this day. Lets just say, KISS is a solid
+principle.
+
+I was trying to avoid writing explicit rules to avoid the non-button square, but by doing so I
+ended up with a bug riddled solution that had way more complexity than I needed. Lesson learned, the
+best way to do things sometimes is the seemingly verbose way.
+
+Oh yeah, and hashmaps for part 2 again, lol.
+
++2 Very late stars (its January 2nd).
+
+See you tomorrow gamers.
--- src/day_twentyone/day_twentyone.zig
+++ src/day_twentyone/day_twentyone.zig
@@ -3,42 +3,27 @@ const file = @import("../file-helpers.zig");
 const Point = @import("../Point.zig").Point;
 
 const Button = struct { input: u8, location: Point };
-const Keypad = struct { buttons: []const Button, disallowed_location: Point };
-
-const NUMBER_PAD = Keypad{
-    .buttons = &[11]Button{
-        .{ .input = '7', .location = Point{ .x = 0, .y = 0 } },
-        .{ .input = '8', .location = Point{ .x = 1, .y = 0 } },
-        .{ .input = '9', .location = Point{ .x = 2, .y = 0 } },
-        .{ .input = '4', .location = Point{ .x = 0, .y = 1 } },
-        .{ .input = '5', .location = Point{ .x = 1, .y = 1 } },
-        .{ .input = '6', .location = Point{ .x = 2, .y = 1 } },
-        .{ .input = '1', .location = Point{ .x = 0, .y = 2 } },
-        .{ .input = '2', .location = Point{ .x = 1, .y = 2 } },
-        .{ .input = '3', .location = Point{ .x = 2, .y = 2 } },
-        .{ .input = '0', .location = Point{ .x = 1, .y = 3 } },
-        .{ .input = 'A', .location = Point{ .x = 2, .y = 3 } },
-    },
-    .disallowed_location = Point{ .x = 0, .y = 3 },
-};
-
-const DIRECTIONAL_PAD = Keypad{
-    .buttons = &[5]Button{
-        .{ .input = '^', .location = Point{ .x = 1, .y = 0 } },
-        .{ .input = 'A', .location = Point{ .x = 2, .y = 0 } },
-        .{ .input = '<', .location = Point{ .x = 0, .y = 1 } },
-        .{ .input = 'v', .location = Point{ .x = 1, .y = 1 } },
-        .{ .input = '>', .location = Point{ .x = 2, .y = 1 } },
-    },
-    .disallowed_location = Point{ .x = 0, .y = 0 },
-};
 
 pub fn run(allocator: std.mem.Allocator) !void {
     const buffer = try allocator.alloc(u8, 25000);
     defer allocator.free(buffer);
     const input_length = try file.readFileToBuffer("input/day21.txt", buffer);
 
-    var lines = std.mem.splitSequence(u8, buffer[0 .. input_length - 1], "\n");
+    std.debug.print(
+        "Total complexity (part 1): {}\n",
+        .{try runWithDepth(buffer[0 .. input_length - 1], 2, allocator)},
+    );
+
+    std.debug.print(
+        "Total complexity (part 2): {}\n",
+        .{try runWithDepth(buffer[0 .. input_length - 1], 25, allocator)},
+    );
+}
+
+fn runWithDepth(input: []const u8, depth: usize, allocator: std.mem.Allocator) !usize {
+    var lines = std.mem.splitSequence(u8, input, "\n");
+    var previous_expansions = std.StringHashMap(usize).init(allocator);
+    defer previous_expansions.clearAndFree();
 
     var total_complexity: usize = 0;
     while (lines.next()) |line| {
@@ -46,168 +31,107 @@ pub fn run(allocator: std.mem.Allocator) !void {
         defer key_sequence.clearAndFree();
 
         var keypad_arm_location = Point{ .x = 2, .y = 3 };
+        var total_key_count: usize = 0;
         for (line) |input_character| {
-            const new_key_sequence = try getNecessaryDirectionalInputs(
+            const new_key_sequence = try getNecessaryDirectionalInputsForNumberPad(
                 keypad_arm_location,
                 input_character,
-                NUMBER_PAD,
                 allocator,
             );
 
-            std.debug.print("Initial input sequence:\n{s}\n", .{new_key_sequence.input_sequence});
-            try key_sequence.appendSlice(try expandInputCode(
+            total_key_count += try getExpandedInputCodeLength(
                 new_key_sequence.input_sequence,
-                NUMBER_PAD,
-                keypad_arm_location,
+                depth,
                 0,
+                &previous_expansions,
                 allocator,
-            ));
+            );
             keypad_arm_location = new_key_sequence.new_location;
         }
 
         const input_code = try key_sequence.toOwnedSlice();
         defer allocator.free(input_code);
 
-        const complexity_to_add = try std.fmt.parseInt(usize, line[0..3], 10) * input_code.len;
-        total_complexity += complexity_to_add;
-
-        std.debug.print("For input {s} got {} complexity key sequence ({s} {})\n{s}\n\n", .{
-            line,
-            complexity_to_add,
-            line[0..3],
-            input_code.len,
-            input_code,
-        });
+        const mult = try std.fmt.parseInt(usize, line[0..3], 10);
+        total_complexity += mult * total_key_count;
     }
 
-    std.debug.print("Total complexity (part 1): {} (< 181454)\n", .{total_complexity});
+    return total_complexity;
 }
 
-const DIRECTIONAL_ARM_COUNT: usize = 2;
-const DIRECTIONAL_ARM_START = Point{ .x = 2, .y = 0 };
-
-fn expandInputCode(input: []u8, previous_keypad: Keypad, previous_arm_start: Point, depth: usize, allocator: std.mem.Allocator) ![]u8 {
-    if (depth == DIRECTIONAL_ARM_COUNT) {
-        return input;
+fn getExpandedInputCodeLength(
+    input: []u8,
+    max_depth: usize,
+    depth: usize,
+    previous_expansions: *std.StringHashMap(usize),
+    allocator: std.mem.Allocator,
+) !usize {
+    if (depth == max_depth) {
+        return input.len;
     }
 
-    var key_sequence = std.ArrayList(u8).fromOwnedSlice(allocator, input);
+    const index_string = try std.fmt.allocPrint(
+        allocator,
+        "{s}_{}",
+        .{ input, depth },
+    );
+
+    if (previous_expansions.get(index_string)) |previous_value| {
+        allocator.free(index_string);
+        return previous_value;
+    }
+
     var character_index: usize = 0;
     var take_count: usize = 0;
-    while (character_index < key_sequence.items.len) {
-        if (key_sequence.items[character_index] != 'A') {
+    var input_total: usize = 0;
+    while (character_index < input.len) {
+        if (input[character_index] != 'A') {
             character_index += 1;
             take_count += 1;
             continue;
         }
 
         if (take_count == 0) {
+            input_total += 1;
             character_index += 1;
             continue;
         }
 
         const start_location = character_index - take_count;
-        const inputs = key_sequence.items[start_location .. character_index + 1];
-        const reordered_inputs = try reorderInputs(inputs, previous_keypad, previous_arm_start, allocator);
+        const inputs = input[start_location .. character_index + 1];
 
-        var expanded_inputs = try getInputSequenceExpansion(DIRECTIONAL_ARM_START, inputs, DIRECTIONAL_PAD, allocator);
+        const expanded_inputs = try getInputSequenceExpansion(inputs, allocator);
+        defer allocator.free(expanded_inputs);
 
-        if (!std.mem.eql(u8, inputs, reordered_inputs)) {
-            std.debug.print("Reorder for {s} is {s}\n", .{ inputs, reordered_inputs });
-            const expanded_reordered_inputs = try getInputSequenceExpansion(DIRECTIONAL_ARM_START, reordered_inputs, DIRECTIONAL_PAD, allocator);
-            const reordered_rating = calculateInputDistanceRating(
-                DIRECTIONAL_ARM_START,
-                expanded_reordered_inputs,
-                DIRECTIONAL_PAD,
-            );
-
-            const inputs_rating = calculateInputDistanceRating(
-                DIRECTIONAL_ARM_START,
-                expanded_inputs,
-                DIRECTIONAL_PAD,
-            );
-
-            std.debug.print("Comparing {s} ({d}) to {s} ({d})\n", .{ expanded_inputs, inputs_rating, expanded_reordered_inputs, reordered_rating });
-
-            if (reordered_rating < inputs_rating) {
-                allocator.free(expanded_inputs);
-                expanded_inputs = expanded_reordered_inputs;
-            }
-        }
-
-        try key_sequence.replaceRange(
-            start_location,
-            inputs.len,
+        input_total += try getExpandedInputCodeLength(
             expanded_inputs,
+            max_depth,
+            depth + 1,
+            previous_expansions,
+            allocator,
         );
 
-        character_index += expanded_inputs.len - take_count;
+        character_index += 1;
         take_count = 0;
     }
 
-    std.debug.print("Arm {} expanded sequence to:\n{s}\n", .{ depth, key_sequence.items });
+    try previous_expansions.put(index_string, input_total);
 
-    return expandInputCode(try key_sequence.toOwnedSlice(), DIRECTIONAL_PAD, DIRECTIONAL_ARM_START, depth + 1, allocator);
+    return input_total;
 }
 
-pub fn reorderInputs(
-    input_sequence: []const u8,
-    previous_keypad: Keypad,
-    previous_arm_start: Point,
-    allocator: std.mem.Allocator,
-) ![]const u8 {
-    var characters = std.ArrayList(u8).init(allocator);
-    var character_counts = std.ArrayList(usize).init(allocator);
-    defer characters.deinit();
-    defer character_counts.deinit();
-
-    for (input_sequence[0 .. input_sequence.len - 1]) |character| {
-        if (characters.items.len != 0 and characters.items[0] == character) {
-            character_counts.items[0] += 1;
-        } else {
-            try characters.insert(0, character);
-            try character_counts.insert(0, 1);
-        }
-    }
-
-    var new_characters = std.ArrayList(u8).init(allocator);
-    for (characters.items, 0..) |next_character, index| {
-        try new_characters.appendNTimes(next_character, character_counts.items[index]);
-    }
-    try new_characters.append('A');
-
-    var location = previous_arm_start;
-    for (new_characters.items) |character| {
-        switch (character) {
-            '<' => location = location.add(Point{ .x = -1, .y = 0 }),
-            '>' => location = location.add(Point{ .x = 1, .y = 0 }),
-            '^' => location = location.add(Point{ .x = 0, .y = -1 }),
-            'v' => location = location.add(Point{ .x = 0, .y = 1 }),
-            else => {},
-        }
-
-        if (location.eq(previous_keypad.disallowed_location)) {
-            new_characters.clearAndFree();
-            return input_sequence;
-        }
-    }
-
-    return try new_characters.toOwnedSlice();
-}
+const DIRECTIONAL_ARM_START = Point{ .x = 2, .y = 0 };
 
 fn getInputSequenceExpansion(
-    start_location: Point,
     input_sequence: []const u8,
-    keypad: Keypad,
     allocator: std.mem.Allocator,
-) ![]const u8 {
+) ![]u8 {
     var new_inputs = std.ArrayList(u8).init(allocator);
-    var current_location = start_location;
+    var current_location = DIRECTIONAL_ARM_START;
     for (input_sequence) |input| {
         const input_result = try getNecessaryDirectionalInputs(
             current_location,
             input,
-            keypad,
             allocator,
         );
 
@@ -218,10 +142,23 @@ fn getInputSequenceExpansion(
     return try new_inputs.toOwnedSlice();
 }
 
-fn getNecessaryDirectionalInputs(
+const NUMBER_PAD = [11]Button{
+    .{ .input = '7', .location = Point{ .x = 0, .y = 0 } },
+    .{ .input = '8', .location = Point{ .x = 1, .y = 0 } },
+    .{ .input = '9', .location = Point{ .x = 2, .y = 0 } },
+    .{ .input = '4', .location = Point{ .x = 0, .y = 1 } },
+    .{ .input = '5', .location = Point{ .x = 1, .y = 1 } },
+    .{ .input = '6', .location = Point{ .x = 2, .y = 1 } },
+    .{ .input = '1', .location = Point{ .x = 0, .y = 2 } },
+    .{ .input = '2', .location = Point{ .x = 1, .y = 2 } },
+    .{ .input = '3', .location = Point{ .x = 2, .y = 2 } },
+    .{ .input = '0', .location = Point{ .x = 1, .y = 3 } },
+    .{ .input = 'A', .location = Point{ .x = 2, .y = 3 } },
+};
+
+fn getNecessaryDirectionalInputsForNumberPad(
     current_location: Point,
     target_button: u8,
-    keypad: Keypad,
     allocator: std.mem.Allocator,
 ) !struct {
     input_sequence: []u8,
@@ -229,55 +166,88 @@ fn getNecessaryDirectionalInputs(
 } {
     var inputs = std.ArrayList(u8).init(allocator);
 
-    var key_location = current_location;
-    if (getKeyLocation(target_button, keypad)) |target_location| {
-        key_location = target_location;
-        const distance = key_location.sub(current_location);
+    const target_location = getKeyLocation(target_button, &NUMBER_PAD);
+    const distance = target_location.sub(current_location);
 
-        if (!current_location.add(Point{ .x = distance.x, .y = 0 }).eq(keypad.disallowed_location)) {
-            var current_key: u8 = '>';
-            if (distance.x < 0) current_key = '<';
-            try inputs.appendNTimes(current_key, @abs(distance.x));
+    var x_key: u8 = '>';
+    if (distance.x < 0) x_key = '<';
+    var y_key: u8 = 'v';
+    if (distance.y < 0) y_key = '^';
 
-            current_key = 'v';
-            if (distance.y < 0) current_key = '^';
-            try inputs.appendNTimes(current_key, @abs(distance.y));
-        } else {
-            var current_key: u8 = 'v';
-            if (distance.y < 0) current_key = '^';
-            try inputs.appendNTimes(current_key, @abs(distance.y));
-
-            current_key = '>';
-            if (distance.x < 0) current_key = '<';
-            try inputs.appendNTimes(current_key, @abs(distance.x));
-        }
+    if (current_location.y == 3 and target_location.x == 0) {
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+        try inputs.appendNTimes(x_key, @abs(distance.x));
+    } else if (current_location.x == 0 and target_location.y == 3) {
+        try inputs.appendNTimes(x_key, @abs(distance.x));
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+    } else if (distance.x < 0) {
+        try inputs.appendNTimes(x_key, @abs(distance.x));
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+    } else {
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+        try inputs.appendNTimes(x_key, @abs(distance.x));
     }
 
     try inputs.append('A');
 
     return .{
         .input_sequence = try inputs.toOwnedSlice(),
-        .new_location = key_location,
+        .new_location = target_location,
     };
 }
 
-fn calculateInputDistanceRating(start_location: Point, input_sequence: []const u8, keypad: Keypad) f64 {
-    var current_location = start_location;
-    var distance_rating: f64 = 0;
-    for (input_sequence) |next_key| {
-        if (getKeyLocation(next_key, keypad)) |next_key_location| {
-            distance_rating += current_location.distance(next_key_location);
-            current_location = next_key_location;
-        }
+const DIRECTIONAL_PAD = [5]Button{
+    .{ .input = '^', .location = Point{ .x = 1, .y = 0 } },
+    .{ .input = 'A', .location = Point{ .x = 2, .y = 0 } },
+    .{ .input = '<', .location = Point{ .x = 0, .y = 1 } },
+    .{ .input = 'v', .location = Point{ .x = 1, .y = 1 } },
+    .{ .input = '>', .location = Point{ .x = 2, .y = 1 } },
+};
+
+fn getNecessaryDirectionalInputs(
+    current_location: Point,
+    target_button: u8,
+    allocator: std.mem.Allocator,
+) !struct {
+    input_sequence: []u8,
+    new_location: Point,
+} {
+    var inputs = std.ArrayList(u8).init(allocator);
+
+    const target_location = getKeyLocation(target_button, &DIRECTIONAL_PAD);
+    const distance = target_location.sub(current_location);
+
+    var x_key: u8 = '>';
+    if (distance.x < 0) x_key = '<';
+    var y_key: u8 = 'v';
+    if (distance.y < 0) y_key = '^';
+
+    if (current_location.x == 0 and target_location.y == 0) {
+        try inputs.appendNTimes(x_key, @abs(distance.x));
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+    } else if (current_location.y == 0 and target_location.x == 0) {
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+        try inputs.appendNTimes(x_key, @abs(distance.x));
+    } else if (distance.x < 0) {
+        try inputs.appendNTimes(x_key, @abs(distance.x));
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+    } else {
+        try inputs.appendNTimes(y_key, @abs(distance.y));
+        try inputs.appendNTimes(x_key, @abs(distance.x));
     }
 
-    return distance_rating;
+    try inputs.append('A');
+
+    return .{
+        .input_sequence = try inputs.toOwnedSlice(),
+        .new_location = target_location,
+    };
 }
 
-fn getKeyLocation(target_button: u8, keypad: Keypad) ?Point {
-    for (keypad.buttons) |key| {
+fn getKeyLocation(target_button: u8, keypad: []const Button) Point {
+    for (keypad) |key| {
         if (key.input == target_button) return key.location;
     }
 
-    return null;
+    unreachable;
 }
